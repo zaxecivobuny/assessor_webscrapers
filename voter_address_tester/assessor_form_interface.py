c2f7e2cfac82d6264b352f2873bcbd909bf96876
Josh <zaxecivobuny@gmail.com>
2020-10-16 23:00:25 -0500

    some cleanup and nesting the file write inside the read

--- voter_address_tester/assessor_form_interface.py
+++ voter_address_tester/assessor_form_interface.py
@@ -15,11 +15,6 @@ def get_owner_name_from_address(address):
     return property_owners
 
 
-def current_test():
-    get_owner_name_from_address('5100 DRESDEN AV')
-    get_owner_name_from_address('4095 WILMINGTON AVE')
-
-
 def main():
     data_file_location = "data/city voters non-apts - CountyWide_VotersList_08202020_.csv" # noQA
     output_file_location = "data/output.csv"
@@ -27,24 +22,21 @@ def main():
     street_number_idx = 3
     street_name_idx = 5
     street_type_idx = 6
-    output_file = ''
 
     with open(data_file_location) as data_file:
-        for line in data_file:
-            output_file += line[:-1]
-            print(line)
-            fields = line.split(',')
-            assembled_address = ''
-            assembled_address += fields[street_number_idx] + ' '
-            assembled_address += fields[street_name_idx] + ' '
-            assembled_address += fields[street_type_idx]
-            print(assembled_address)
-            property_owners = get_owner_name_from_address(assembled_address)
-            output_file += property_owners + '\n'
-            # print(output_file)
-
-    with open(output_file_location, 'w') as o:
-        o.write(output_file)
+        with open(output_file_location, 'w') as o:
+            for line in data_file:
+                output_line = line[:-1]
+                print(line)
+                fields = line.split(',')
+                assembled_address = ''
+                assembled_address += fields[street_number_idx] + ' '
+                assembled_address += fields[street_name_idx] + ' '
+                assembled_address += fields[street_type_idx]
+                print(assembled_address)
+                owners = get_owner_name_from_address(assembled_address)
+                output_line += owners + '\n'
+                o.write(output_line)
 
 
 if __name__ == '__main__':
